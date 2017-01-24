cb43910454efe728606ade1dca2d42251f638d1f
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-01-24 12:02:34 +0000

    adding Membership->Contract word replacement

--- managed_entities.mgd.php
+++ managed_entities.mgd.php
@@ -1,5 +1,6 @@
 <?php
-return array(array(
+return array(
+  array(
   'entity' => 'OptionValue',
   'name' => 'activity_type.contract_history',
   'params' => array(
@@ -7,5 +8,13 @@ return array(array(
     'label' => 'Contract history',
     'name' => 'Contract history',
     'option_group_id' => 'activity_type'
-  )
-));
+  )),
+  array(
+  'entity' => 'WordReplacement',
+  'name' => 'Membership->Contract',
+  'params' => array(
+    'version' => '3',
+    'find_word' => 'Membership',
+    'replace_word' => 'Contract',
+  ))
+);
