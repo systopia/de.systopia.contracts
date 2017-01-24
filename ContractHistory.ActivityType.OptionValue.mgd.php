6cd2cfccc5c7df2fcb83cd916aee5814a5c0ca13
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-01-24 11:53:36 +0000

    creating "contract history activity type option value" managed entity

---
+++ ContractHistory.ActivityType.OptionValue.mgd.php
@@ -0,0 +1,11 @@
+<?php
+return array(array(
+  'entity' => 'OptionValue',
+  'name' => 'activity_type.contract_history',
+  'params' => array(
+    'version' => '3',
+    'label' => 'Contract history',
+    'name' => 'Contract history',
+    'option_group_id' => 'activity_type'
+  )
+));
