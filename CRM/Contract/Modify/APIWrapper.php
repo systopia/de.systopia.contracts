b39a53a0c521e464bdc782498a96933beb051ff0
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-02-21 14:09:47 +0100

    reducing scope of API wrapper as we'll handle the recording of activities in the pre+post hooks

--- CRM/Contract/Modify/APIWrapper.php
+++ CRM/Contract/Modify/APIWrapper.php
@@ -1,35 +1,23 @@
 <?php
 
-class CRM_Contract_HistoryApiWrapper{
+class CRM_Contract_Modify_APIWrapper{
 
   private static $_singleton;
 
-  /**
-   * I've made the contsructor private but TBH it might be that sometimes it's
-   * useful/necessary to create a new class, esp. if we start updating many
-   * contracts in the same script, e.g. through an import. Might be another
-   * patter we can use.
-   */
   private function __construct(){
     $this->contractHandler = new CRM_Contract_Handler();
   }
 
   public static function singleton() {
     if (!self::$_singleton) {
-      self::$_singleton = new CRM_Contract_HistoryApiWrapper();
+      self::$_singleton = new CRM_Contract_Modify_APIWrapper();
     }
     return self::$_singleton;
   }
 
   function fromApiInput($apiRequest){
 
-    // force a reload to help with debugging
-    // TODO decide whether we want to turn this off by default for performance reasons
-    if(!isset($apiRequest['params']['options']['reload'])){
-      $apiRequest['params']['options']['reload'] = 1;
-    }
-
-    // We only need to take action here if
+    // We only need to take action here if this is an edit
     if(isset($apiRequest['params']['id'])){
       $this->contractHandler->setStartMembership($apiRequest['params']['id']);
       $this->contractHandler->addProposedParams($apiRequest['params']);
@@ -37,10 +25,8 @@ class CRM_Contract_HistoryApiWrapper{
         throw new \CiviCRM_API3_Exception("Cannot update contract status from {$this->contractHandler->startStatus} to {$this->contractHandler->desiredEndStatus}.");
       }
       if(!$this->contractHandler->isValidFieldUpdate()){
-        //TODO Better way to use exceptions here?
         throw new \CiviCRM_API3_Exception($this->contractHandler->errorMessage);
       }
-      // TODO some membership status changes (e.g. to Cancelled or Paused) do not allow us to change membership fields. check to see that we are not trying to change membership fields if changing to these particular statuses
     }
     return $apiRequest;
   }
