bb7a1c98e2652bcea8edd526c7e8d2709fbdf613
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-03-15 13:53:39 +0000

    hide membership payment blocks on form

--- js/membership-edit.js
+++ js/membership-edit.js
@@ -1,4 +1,6 @@
 CRM.$(function($) {
+  $('#contri').hide();
+  $('select#status_id option').hide();
   $('.crm-membership-form-block-record_contribution').hide();
   $('select#status_id option:contains(- select -)').show();
   $('select#status_id option:contains(Current)').show();
