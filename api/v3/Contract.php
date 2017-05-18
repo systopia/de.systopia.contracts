013854f3a641ad63910f3b9a68d2cac1a5af0e83
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-05-18 15:34:37 +0100

    contract.create should add a signed activity, and other typos

--- api/v3/Contract.php
+++ api/v3/Contract.php
@@ -1,14 +1,10 @@
 <?php
-function _civicrm_api3_Contract_modify_spec(&$params){
-  $params['id']['api.required'] = 1;
-  $params['action']['api.required'] = 1;
-}
 
 
-// A wrapper around Membership.create with appropriate fields passed.
+// A wrapper around Membership.create with appropriate fields passed. On cannot
+// schedule Contract.create for the future.
 
 function civicrm_api3_Contract_create($params){
-
     // Any parameters with a period in will be converted to the custom_N format
     // Other fields will be passed directly to the membership.create API
     foreach ($params as $key => $value){
@@ -18,18 +14,38 @@ function civicrm_api3_Contract_create($params){
         $params[CRM_Contract_Utils::getCustomFieldId($key)] = $value;
       }
     }
-    return civicrm_api3('Membership', 'create', $params);
+    $membership = civicrm_api3('Membership', 'create', $params);
+
+    // Add a signed activity once this contract has been created
+    $membershipParams = $membership['values'][$membership['id']];
+    $sign = new CRM_Contract_ModificationActivity_Sign;
+    $activityParams['status_id'] = 'completed';
+    $activityParams['activity_type_id'] = $sign->getActivityType();
+    $activityParams['activity_date_time'] = $membershipParams['join_date'];
+    $activityParams['source_record_id'] = $membershipParams['id'];
+    $activityParams['medium_id'] = $params['medium_id'];
+    $activityParams['description'] = $params['note'];
+    $activityParams['source_contact_id'] = $membershipParams['contact_id'];
+
+    $activity = civicrm_api3('Activity', 'create', $activityParams);
+
+    return $membership;
 }
 
-// A wrapper around Activity.create of an contract modification activity.
-// Contract.modify enables the updating of contracts either now or in the future
+function _civicrm_api3_Contract_modify_spec(&$params){
+  $params['id']['api.required'] = 1;
+  $params['action']['api.required'] = 1;
+}
 
+// A wrapper around Activity.create of an contract modification activity (which
+// in turn wraps Membership.create). Contract.modify enables the updating of
+// contracts either now or in the future.
 function civicrm_api3_Contract_modify($params){
 
   // Throw an exception if the date is in the past. Rational:
   // This model requires being able to compare the pre and post state of the
-  // contract to create accurate changes. It would be hard to insert them in to
-  // the past.
+  // contract to create accurate changes. It would require a lot of logic and
+  // manipulation of existing data to be able add modifications retrospectivley.
   $date = new DateTime($params['date']);
   if($date < new DateTime()){
     throw new Exception("'date' must either be in the future, or absent if you want to execute the modification immediatley.");
@@ -44,12 +60,12 @@ function civicrm_api3_Contract_modify($params){
   $activityParams['activity_date_time'] = (new DateTime($params['date']))->format('Y-m-d H:i:s');
   $activityParams['source_record_id'] = $params['id'];
   $activityParams['medium_id'] = $params['medium_id'];
-  $activityParams['description'] = $params['note'];
+  $activityParams['details'] = $params['note'];
 
   // Find the contact that this membership is associated with so we can
   // associate the activity
-  $membership = civicrm_api3('membership', 'getsingle', ['id' => $params['id']]);
-  $activityParams['source_contact_id'] = $membership['contact_id'];
+  $membershipParams = civicrm_api3('membership', 'getsingle', ['id' => $params['id']]);
+  $activityParams['source_contact_id'] = $membershipParams['contact_id'];
 
   // Depending on the activity type, populate more parameters / do extra
   // processing
@@ -69,7 +85,7 @@ function civicrm_api3_Contract_modify($params){
       break;
     case 'cancel':
       if(isset($params['membership_cancellation.membership_cancel_reason'])){
-        $activityParams[CRM_Contract_Utils::contractToActivityCustomFieldId('membership_cancellation.membership_cancel_reason')] = $params['membership.membership_cancellation.membership_cancel_reason'];
+        $activityParams[CRM_Contract_Utils::contractToActivityCustomFieldId('membership_cancellation.membership_cancel_reason')] = $params['membership_cancellation.membership_cancel_reason'];
       }
       break;
     case 'pause':
@@ -78,7 +94,7 @@ function civicrm_api3_Contract_modify($params){
           'status_id' => 'scheduled',
           'source_record_id' => $params['id'],
           'activity_type_id' => 'Contract_Resumed',
-          'source_contact_id' => $membership['contact_id'],
+          'source_contact_id' => $membershipParams['contact_id'],
           'activity_date_time' => (new DateTime($params['resume_date']))->format('Y-m-d H:i:s'),
         ]);
       }else{
