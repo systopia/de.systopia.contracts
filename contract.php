e9ab86f30fa82dcf72d3317ed97b79f72e8e622c
systopia <endres@systopia.de>
2017-05-31 07:33:09 +0200

    GP-683 (new activity status)

--- contract.php
+++ contract.php
@@ -63,6 +63,7 @@ function contract_civicrm_enable() {
   require_once 'CRM/Contract/CustomData.php';
   $customData = new CRM_Contract_CustomData('de.systopia.contract');
   $customData->syncOptionGroup(__DIR__ . '/resources/activity_types_option_group.json');
+  $customData->syncOptionGroup(__DIR__ . '/resources/activity_status_option_group.json');
   $customData->syncCustomGroup(__DIR__ . '/resources/contract_cancellation_custom_group.json');
   $customData->syncCustomGroup(__DIR__ . '/resources/contract_updates_custom_group.json');
   $customData->syncEntities(__DIR__ . '/resources/membership_status_entities.json');
