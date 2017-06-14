06654c4272d56cbd6ce9c552dbb39d5fb49e6c3d
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-14 18:28:53 +0100

    first draft combined history and upcoming modifications page

--- CRM/Contract/Page/Review.php
+++ CRM/Contract/Page/Review.php
@@ -3,30 +3,11 @@
 class CRM_Contract_Page_Review extends CRM_Core_Page {
 
   public function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     if(!$id = CRM_Utils_Request::retrieve('id', 'Positive')){
       Throw new Exception('Missing a valid contract ID');
     }
 
-    // Get activity statuses
-    $activityStatuses = civicrm_api3('OptionValue', 'get', [ 'option_group_id' => "activity_status"]);
-    foreach(civicrm_api3('OptionValue', 'get', [ 'option_group_id' => 'activity_status', 'return' => ['value', 'label'] ])['values'] as $activityStatus){
-      $activityStatuses[$activityStatus['value']] = $activityStatus['label'];
-    }
-    $this->assign('activityStatuses', $activityStatuses);
-
-    // Get activity types
-    $this->assign('activityTypes', CRM_Contract_ModificationActivity::getModificationActivityTypeLabels());
-    $this->assign('includeWysiwygEditor', true);
-
-    // Get membership types
-    $membershipTypes = civicrm_api3('MembershipType', 'get')['values'];
-    $this->assign('membershipTypes', $membershipTypes);
-
-    // Get campaign types
-    $campaigns = civicrm_api3('Campaign', 'get')['values'];
-    $this->assign('campaigns', $campaigns);
-
+    // Set activity params
     $activityParams = [
       'source_record_id' => $id,
       'status_id' => ['NOT IN' => ['cancelled']],
@@ -35,29 +16,53 @@ class CRM_Contract_Page_Review extends CRM_Core_Page {
         'status_id',
         'activity_type_id',
         'target_contact_id',
-        'campaign_id'
+        'source_contact_id',
+        'details',
+        'campaign_id',
+        'medium_id'
       ],
     ];
-
-    // Get activities
     foreach(civicrm_api3('CustomField', 'get', [ 'custom_group_id' => ['IN' => ['contract_cancellation', 'contract_updates']]])['values'] as $customField){
       $activityParams['return'][]='custom_'.$customField['id'];
     }
+    $activities = civicrm_api3('Activity', 'get', $activityParams)['values'];
+
 
     // Friendlify custom field names
     CRM_Contract_Utils::warmCustomFieldCache();
     $customFieldIndex = array_flip(CRM_Contract_Utils::$customFieldCache);
     $customFieldIndex = str_replace('.', '_', $customFieldIndex);
-    $activities = civicrm_api3('Activity', 'get', $activityParams)['values'];
+
+    // To collect the campaign ids that we need to get the names of
+    $campaigns=[];
+    $contacts=[];
+
+
+
     foreach($activities as $key => $activity){
 
       // For accurate sorting by time
       $activities[$key]['activity_date_unixtime'] = strtotime($activity['activity_date_time']);
+
       foreach($activity as $innerKey => $field){
         if(isset($customFieldIndex[$innerKey])){
           unset($activities[$key][$innerKey]);
           $activities[$key][$customFieldIndex[$innerKey]] = $field;
         }
+      }
+      if(isset($activities[$key]['contract_updates_ch_recurring_contribution']) && $activities[$key]['contract_updates_ch_recurring_contribution']){
+        civicrm_api3('ContributionRecur', 'getsingle', ['id' => $activities[$key]['contract_updates_ch_recurring_contribution']]);
+        $c = new CRM_Contract_RecurringContribution;
+        $activities[$key]['contract_updates_ch_recurring_contribution_text'] = $c->writePaymentContractLabel( civicrm_api3('ContributionRecur', 'getsingle', ['id' => $activities[$key]['contract_updates_ch_recurring_contribution']]));
+      }
+      if(isset($activities[$key]['contract_updates_ch_annual']) && isset($activities[$key]['contract_updates_ch_frequency']) && $activities[$key]['contract_updates_ch_annual'] && $activities[$key]['contract_updates_ch_frequency']){
+        $activities[$key]['contract_updates_ch_amount'] = $activities[$key]['contract_updates_ch_annual'] / $activities[$key]['contract_updates_ch_frequency'];
+      }
+      if(isset($activities[$key]['campaign_id'])){
+        $campaigns[] = $activities[$key]['campaign_id'];
+      }
+      if(isset($activities[$key]['source_contact_id'])){
+        $contacts[] = $activities[$key]['source_contact_id'];
       }
     }
 
@@ -66,10 +71,52 @@ class CRM_Contract_Page_Review extends CRM_Core_Page {
       return $b['activity_date_unixtime'] - $a['activity_date_unixtime'];
     });
 
-    // var_dump(CRM_Contract_Utils::$customFieldCache);
-    // var_dump($activityParams);
-
     $this->assign('activities', $activities);
+
+    // Get campaigns
+    if($campaigns){
+      foreach(civicrm_api3('Campaign', 'get', ['id' => ['IN' => array_unique($campaigns)]])['values'] as $campaign){
+        $campaigns[$campaign['id']] = $campaign['name'];
+      }
+    }
+    $this->assign('campaigns', $campaigns);
+
+    foreach(civicrm_api3('Contact', 'get', ['id' => ['IN' => array_unique($contacts)]])['values'] as $contact){
+      $contacts[$contact['id']] = $contact['display_name'];
+    }
+    $this->assign('contacts', $contacts);
+
+    foreach(civicrm_api3('OptionValue', 'get', [ 'option_group_id' => 'encounter_medium', 'return' => ['value', 'label'] ])['values'] as $medium){
+      $mediums[$medium['value']] = $medium['label'];
+    }
+    $this->assign('mediums', $mediums);
+
+
+    // Get activity statuses
+    foreach(civicrm_api3('OptionValue', 'get', [ 'option_group_id' => 'activity_status', 'return' => ['value', 'label'] ])['values'] as $activityStatus){
+      $activityStatuses[$activityStatus['value']] = $activityStatus['label'];
+    }
+    $this->assign('activityStatuses', $activityStatuses);
+
+    foreach(civicrm_api3('OptionValue', 'get', [ 'option_group_id' => 'payment_frequency', 'return' => ['value', 'label'] ])['values'] as $paymentFrequency){
+      $paymentFrequencies[$paymentFrequency['value']] = $paymentFrequency['label'];
+    }
+    $this->assign('paymentFrequencies', $paymentFrequencies);
+
+    // Get activity types
+    $this->assign('activityTypes', CRM_Contract_ModificationActivity::getModificationActivityTypeLabels());
+    $this->assign('includeWysiwygEditor', true);
+
+    // Get membership types
+    foreach(civicrm_api3('MembershipType', 'get', [])['values'] as $membershipType){
+      $membershipTypes[$membershipType['id']] = $membershipType['name'];
+    }
+    $this->assign('membershipTypes', $membershipTypes);
+
+    $this->assign('membershipTypes', $membershipTypes);
+
+
+
 
     CRM_Core_Resources::singleton()->addScriptFile('civicrm', 'packages/ckeditor/ckeditor.js');
     foreach(civicrm_api3('CustomField', 'get', [ 'custom_group_id' => ['IN' => ['contract_cancellation', 'contract_updates']]])['values'] as $customField){
--- templates/CRM/Contract/Page/Review.tpl
+++ templates/CRM/Contract/Page/Review.tpl
@@ -1,33 +1,46 @@
 <table>
   <tr>
-    <th>Date</th>
+
     <th>Modification</th>
-    <th>Campaign</th>
-    <th>Type</th>
-    <th>Mandate</th>
-    <th>Annual amount</th>
+    <th>Date</th>
+    <th>Payment</th>
+    <th>Amount</th>
+
     <th>Frequency</th>
-    <th>Bank account</th>
     <th>Cycle day</th>
+    <th>Type</th>
+    <th>Campaign</th>
+
+    <th>Medium</th>
+    <th>Note</th>
     <th>Cancel reason</th>
+    <th>Added by</th>
+
     <th>Status</th>
     <th>Edit</th>
-{foreach from=$activities item=a}
-  <tr class="{if $activityStatuses[$a.status_id] eq 'Needs Review'}needs-review{/if} {if $activityStatuses[$a.status_id] eq 'Scheduled'}scheduled{/if}">
-    <td>{$a.activity_date_time|crmDate}</td>
-    <td>{$activityTypes[$a.activity_type_id]}</td>
-    <td>{$campaigns[$a.campaign_id].name}</td>
-    <td>{$membershipTypes[$a.contract_updates_ch_membership_type].name}</td>
-    <td>{$a.contract_updates_ch_recurring_contribution}</td>
-    <td>{$a.contract_updates_ch_annual}</td>
-    <td>{$a.contract_updates_ch_frequency}</td>
-    <td>{$a.contract_updates_ch_to_ba}</td>
-    <td>{$a.contract_updates_ch_cycle_day}</td>
-    <td>{$a.contract_cancellation_contact_history_cancel_reason}</td>
-    <td>{$activityStatuses[$a.status_id]}</td>
-    <td>
-      <a class="edit-activity" href="{crmURL p='civicrm/activity/add' q="&action=update&reset=1&id=`$a.id`&context=activity&searchContext=activity&cid=`$a.target_contact_id.0`"}" class="create-mandate">edit</a>
-    </td>
+
   </tr>
-{/foreach}
+
+  {foreach from=$activities item=a}
+    <tr class="{if $activityStatuses[$a.status_id] eq 'Needs Review'}needs-review{/if} {if $activityStatuses[$a.status_id] eq 'Scheduled'}scheduled{/if}">
+
+      <td>{$activityTypes[$a.activity_type_id]}</td>
+      <td>{$a.activity_date_time|crmDate}</td>
+      <td>{$a.contract_updates_ch_recurring_contribution_text}</td>
+      <td>{$a.contract_updates_ch_amount}</td>
+
+      <td>{$paymentFrequencies[$a.contract_updates_ch_frequency]}</td>
+      <td>{$a.contract_updates_ch_cycle_day}</td>
+      <td>{$membershipTypes[$a.contract_updates_ch_membership_type]}</td>
+      <td>{$campaigns[$a.campaign_id]}</td>
+
+      <td>{$mediums[$a.medium_id]}</td>
+      <td>{$a.details}</td>
+      <td>{$a.contract_cancellation_contact_history_cancel_reason}</td>
+      <td><a href="{crmURL p='civicrm/contact/view' q="reset=1&cid=`$a.source_contact_id`"}">{$contacts[$a.source_contact_id]}</a></td>
+
+      <td>{$activityStatuses[$a.status_id]}</td>
+      <td>{if $activityStatuses[$a.status_id] neq 'Completed'} <a class="edit-activity" href="{crmURL p='civicrm/activity/add' q="action=update&reset=1&id=`$a.id`&context=activity&searchContext=activity&cid=`$a.target_contact_id.0`"}" class="create-mandate">edit</a> {/if}</td>
+    </tr>
+  {/foreach}
 </table>
