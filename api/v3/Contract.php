20e67c019993f19c56a3eca966601429247e347c
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-21 20:32:53 +0100

    record medium and notes in the contract signed activity

--- api/v3/Contract.php
+++ api/v3/Contract.php
@@ -16,6 +16,16 @@ function civicrm_api3_Contract_create($params){
       }
     }
     $membership = civicrm_api3('Membership', 'create', $params);
+
+    $activity = civicrm_api3('Activity', 'getsingle', [
+      'source_record_id' => $membership['id'],
+      'activity_type_id' => 'Contract_Signed',
+    ]);
+    $activity = civicrm_api3('Activity', 'create', [
+      'id' => $activity['id'],
+      'details' => $params['note'],
+      'medium_id' => $params['medium_id']
+    ]);
     return $membership;
 }
 
