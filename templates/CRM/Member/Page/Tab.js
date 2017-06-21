43530445adbfec754369d1bf1115e903d6e2b3ab
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-06-21 20:33:18 +0100

    refactor review.js. ensure that listeners are only added once

--- templates/CRM/Member/Page/Tab.js
+++ templates/CRM/Member/Page/Tab.js
@@ -6,124 +6,106 @@ CRM.$(function($) {
   $('a[href="/civicrm/contact/view/membership?reset=1&action=add&cid=' + CRM.vars['de.systopia.contract'].cid + '&context=membership"]')
   .attr("href", "/civicrm/contract/create?cid=" + CRM.vars['de.systopia.contract'].cid);
 
-  // # Membership rows should be decorated with more information about the contract #
-
-  // Decorate each row on initial load
-  $( "#memberships tr.crm-membership" ).each(decorateRow);
-
-  // Set modification edit activities to open in a pop up
-  $(document).on( 'click', '.edit-activity', CRM.popup);
-
-  // When a pop up is closed, update the contract status and decorate the row
-  $(document).on( "crmPopupFormSuccess", '.edit-activity', function(){
-    row = $(this).parent().parent().parent().parent().parent().parent().parent().prev();
-    console.log(row);
-    id = getIdFromRow(row);
-    table = $(row).parent();
-    reviewPane = table.find(".crm-membership-review_"+ id)[0];
-    reviewLink = table.find("#crm-membership_"+ id + ' .review-link')[0];
-    $(reviewPane).find('td').load(reviewLink.href);
-
-    CRM.api3('Contract', 'get_open_modification_counts', {
-      "id": id
-    }).done(function(result) {
-      contractStatuses[id] = result;
-      table = $(row).parent();
-      reviewPane = table.find(".crm-membership-review_"+ id)[0];
-      $(reviewPane).find('td').load(reviewLink.href);
-
-    });
+  // Add a membership-review tr with appropriate colspan under each row to
+  // receive the contract history
+  $( "#memberships tr.crm-membership, #inactive-memberships tr.crm-membership" ).each( function(){
+    var elementId = $(this).attr('id');
+    var membershipId = elementId.substr(elementId.indexOf("_") + 1);
+    var link = " <a class='toggle-review' id='crm-membership-review-link_" + membershipId + "' href='/civicrm/contract/review?reset=&snippet=1&id=" + membershipId + "'>" + getReviewLinkText(membershipId) + "</a>";
+    $(this).after("<tr class='crm-membership crm-membership-review odd odd-row' id='crm-membership-review_" + membershipId + "'><td colspan='" + $(this).find('td').length + "'></td></tr>");
+    $(this).find('td.crm-membership-status').append(link);
+    decorateRow(membershipId);
   });
 
-  // Clicking on show review opens up a review pane under the contract if not already open
-  $(document).on( "click", ".review-link", function(e) {
+  // hide the .membership-review tr by default
+  $( "tr.crm-membership-review" ).hide();
 
-    e.preventDefault();
-    var row = $(this).parent().parent();
-    id = getIdFromRow(row);
-    table = $(row).parent();
-
-    reviewPane = table.find(".crm-membership-review_"+ id)[0];
-    reviewLink = table.find("#crm-membership_"+ id + ' .review-link')[0];
-
-    if($(reviewPane).is(":visible")){
-      $(reviewPane).hide();
-      $(reviewLink).html(getReviewLinkText(id));
-    }else{
-      $(reviewPane).find('td').load(reviewLink.href, function(){
-        $(reviewPane).show();
-        $(reviewLink).html('(close)');
-      });
+  function getReviewLinkText(membershipId){
+    if(contractStatuses[membershipId].needs_review > 0){
+      return 'needs review';
+    }else if(contractStatuses[membershipId].scheduled > 0) {
+      return 'scheduled modifications';
     }
+    return 'review';
+  }
 
-    // Prevent the default link action
-
-    // console.log(reviewPane);
-    // reviewPane.each().remove();
-  });
-
-  // Clicking on hide review closes the review pane
-  $(document).on( "click", ".hide-review", function() {
-    var review = $(this).parent().parent();
-    $(review).remove();
-  });
-
-  function decorateRow(){
-    console.log('called decorateRow');
-    // get the status
-    id = getIdFromRow(this);
-    var contractStatus = contractStatuses[id];
-
-    // Add a needs review
-    addReviewHtml.call(this);
+  function decorateRow(membershipId){
+    var contractStatus = contractStatuses[membershipId];
+    var membershipRow = $(document).find('#crm-membership_' + membershipId);
 
     if(contractStatus.needs_review > 0){
-      $(this).addClass('needs-review');
+      membershipRow.addClass('needs-review');
     }else{
-      $(this).removeClass('needs-review');
+      membershipRow.removeClass('needs-review');
     }
 
     if(contractStatus.scheduled > 0){
-      $(this).addClass('scheduled');
+      membershipRow.addClass('scheduled');
     }else{
-      $(this).removeClass('scheduled');
+      membershipRow.removeClass('scheduled');
     }
   }
 
-  function getIdFromRow(row){
-    var elementId = $(row).attr('id');
-    return elementId.substr(elementId.indexOf("_") + 1);
+  function toggleReviewPane(membershipId){
+    console.log('toggle time');
+    var reviewRow = $(document).find('#crm-membership-review_' + membershipId);
+    var reviewLink = $(document).find('#crm-membership-review-link_' + membershipId);
+    if(reviewRow.is(":visible")){
+      reviewRow.hide();
+      reviewLink.html(getReviewLinkText(membershipId));
+    }else{
+      reviewRow.find('td').load('/civicrm/contract/review?reset=&snippet=1&id=' + membershipId, function(){
+        reviewRow.show();
+        reviewLink.html('hide');
+      });
+    }
   }
 
-  function addReviewHtml(){
-
-    id = getIdFromRow(this);
-
-    // Add text to the membership status field if it is not there already
-    if($( this ).find('td.crm-membership-status .review-link').length == 0){
-      console.log('adding the review row');
-      $( this ).find('td.crm-membership-status').append(
-        " <a href='/civicrm/contract/review?reset=&snippet=1&id=" +
-        id + "' class='review-link'>" + getReviewLinkText(id) + "</a>"
-      );
-    }
-
-    if($( this ).parent().find('tr.crm-membership-review_' + id).length == 0){
-      $( this ).after("<tr class= 'crm-membership-review_" + id + "'><td colspan=9></td></tr>");
-    }
-    $( this ).parent().find('tr.crm-membership-review_' + id).hide();
-
+  function updateReviewPane(membershipId){
+    var reviewRow = $(document).find('#crm-membership-review_' + membershipId);
+    reviewRow.find('td').load('/civicrm/contract/review?reset=&snippet=1&id=' + membershipId);
   }
 
-  function getReviewLinkText(id){
-    if(contractStatuses[id].needs_review > 0){
-      text = '(needs review)';
-    }else if(contractStatuses[id].scheduled > 0) {
-      text = '(scheduled modifications)';
-    } else {
-      text = '(view modifications)';
-    }
-    return text;
+  function updateMembershipRow(membershipId){
+    CRM.api3('Contract', 'get_open_modification_counts', {
+      "id": membershipId
+    }).done(function(result) {
+      contractStatuses[membershipId] = result;
+      decorateRow(membershipId);
+    });
   }
 
+  // We have to ensure the listeners are only added once (this seems like a
+  // fairly mad way to do that but anyhow...)
+
+  if(CRM.vars['de.systopia.contract'].listenersLoaded !== true){
+
+    // Clicking on the toggle-review link toggles the membership-review tr
+    $(document).on( 'click', '.toggle-review', function(e){
+      e.preventDefault();
+      var elementId = $(this).attr('id');
+      var membershipId = elementId.substr(elementId.indexOf("_") + 1);
+      toggleReviewPane(membershipId);
+    });
+
+    // Modification edit activities open in a pop up
+    $(document).on( 'click', '.edit-activity', CRM.popup);
+
+    // When modification edit activity pop ups are closed, update the appropriate
+    // membership review pane and membership row
+    $(document).on( 'crmPopupFormSuccess', '.edit-activity', function(){
+      var elementId = $(this).parents('.crm-membership-review').attr('id');
+      var membershipId = elementId.substr(elementId.indexOf("_") + 1);
+      updateReviewPane(membershipId);
+      updateMembershipRow(membershipId);
+    });
+
+    $(document).on( 'crmPopupFormSuccess', '.action-item', function(){
+      var elementId = $(this).parents('.crm-membership').attr('id');
+      var membershipId = elementId.substr(elementId.indexOf("_") + 1);
+      console.log(membershipId);
+    });
+
+    CRM.vars['de.systopia.contract'].listenersLoaded = true;
+  }
 });
