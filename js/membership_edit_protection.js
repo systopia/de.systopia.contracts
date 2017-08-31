25c0192ba67ddc9ba8d52f38e596a37ba0945ba5
systopia <endres@systopia.de>
2017-08-31 12:50:57 +0200

    GP-814 proposed fix

--- js/membership_edit_protection.js
+++ js/membership_edit_protection.js
@@ -5,11 +5,41 @@
 | http://www.systopia.de/                                      |
 +--------------------------------------------------------------*/
 
-cj(document).ready(function() {
+/**
+ * removes a select element's not-selected options.
+ * therfore rendering it virtually "read-only"
+ */
+function removeOtherOptions(index, select_element) {
+  var select = cj(select_element);
+  var current_value = select.val();
+  select.find('option')
+        .filter(function() {
+                  return cj(this).val() != current_value;
+                })
+        .remove();
+}
+
+/**
+ * This will try and protect all monitored fields from being edited
+ */
+function protectMonitoredFields() {
   // quick-n-dirty: disable protected fields in membership edit form
-  cj(".custom-group-membership_payment").addClass("collapsed");
-  cj(".custom-group-membership_payment").find("select").prop('disabled', true);
-  cj(".custom-group-membership_payment").find("input").prop('disabled', true);
-  cj("select[name^=membership_type_id]").prop('disabled', true);
-  cj("input[id=is_override]").parent().parent().hide();
-});
+  var payment_group = cj(".custom-group-membership_payment");
+  var edit_popup = payment_group.closest("div.ui-dialog");
+
+  // make the memership ID selecter "read only"
+  edit_popup.find("select[name^=membership_type_id]").each(removeOtherOptions);
+
+  // remove the status override
+  edit_popup.find("input[id=is_override]").parent().parent().hide();
+
+
+  // do the same for the whole payment_group
+  payment_group.addClass("collapsed");
+  payment_group.find("select").each(removeOtherOptions);
+  payment_group.find("input").prop('readonly', true);
+}
+
+// trigger
+cj(document).ready(protectMonitoredFields);
+cj(document).bind("ajaxComplete", protectMonitoredFields);
