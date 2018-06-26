0c72b34e469342c040a8d61a6f6e2d40be17d6ef
Patrick Figel <pfigel@greenpeace.org>
2018-06-26 10:03:00 +0200

    Use buffer days configuration from SEPA extension

--- CRM/Contract/SepaLogic.php
+++ CRM/Contract/SepaLogic.php
@@ -464,7 +464,8 @@ class CRM_Contract_SepaLogic {
    * @return int next valid cycle day
    */
   public static function nextCycleDay() {
-    $buffer_days = 2; // TODO: more?
+    $creditor = CRM_Sepa_Logic_Settings::defaultCreditor();
+    $buffer_days = (int) CRM_Sepa_Logic_Settings::getSetting("pp_buffer_days") + (int) CRM_Sepa_Logic_Settings::getSetting("batching.FRST.notice", $creditor->id);
     $cycle_days = self::getCycleDays();
 
     $safety_counter = 32;
