523f2b73180f8364687dd46b512a774c0be7d5dc
B. Endres <endres@systopia.de>
2020-02-18 21:47:17 +0100

    [#36] added compatibility test: cancel subject

--- tests/phpunit/CRM/Contract/CompatibilityTest.php
+++ tests/phpunit/CRM/Contract/CompatibilityTest.php
@@ -54,6 +54,35 @@ class CRM_Contract_CompatibilityTest extends CRM_Contract_ContractTestBase {
   }
 
   /**
+   * Check if the subject of the change activity is as requested
+   *
+   * @see https://redmine.greenpeace.at/issues/1276#note-74
+   */
+  public function testCancelActivitySubject() {
+    // create a new contract
+    $contract = $this->createNewContract([
+        'is_sepa'            => 1,
+        'amount'             => '12.00',
+        'frequency_unit'     => 'month',
+        'frequency_interval' => '1',
+        'cycle_day'          => 25,
+        'iban'               => 'DE89370400440532013000',
+        'bic'                => 'GENODEM1GLS',
+    ]);
+
+    // cancel contract
+    $this->modifyContract($contract['id'], 'cancel', 'tomorrow', [
+        'membership_cancellation.membership_cancel_reason' => 'Unknown'
+    ]);
+
+    // get the resulting change activity
+    $this->runContractEngine($contract['id'], '+2 days');
+    $change_activity = $this->getLastChangeActivity($contract['id']);
+    $this->assertNotEmpty($change_activity, "There should be a change activity after the upgrade");
+    $this->assertContains("Unknown", $change_activity['subject'], "Activity subject should contain the cancel reason");
+  }
+
+  /**
    * Check if the system created activities are suppressed (if enabled)
    *
    * @see https://redmine.greenpeace.at/issues/1276#note-74
